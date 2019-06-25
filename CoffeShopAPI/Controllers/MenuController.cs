7abaea26db5283d1ac725563f0dd607a21576e2f
ardotrtrian <ardotrtrian@gmail.com>
2019-06-25 18:39:45 +0400

    Adding First Controller

---
+++ CoffeShopAPI/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeShopAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private EspressoDbContext _db;
+        public MenuController(EspressoDbContext espressoDbContext)
+        {
+            _db = espressoDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var menus = _db.Menus.Include(menu=>menu.SubMenus);
+            return Ok(menus);
+        }
+    }
+}
---
+++ CoffeShopAPI/Data/EspressoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoffeShopAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeShopAPI.Data
+{
+    public class EspressoDbContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public EspressoDbContext(DbContextOptions<EspressoDbContext> options):base(options)
+        {
+
+        }
+    }
+}
